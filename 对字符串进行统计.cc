2e0e9ff3e20427e070c656bf22d23dc71214db63
YM <2672983546@qq.com>
2023-04-29 17:22:04 +0800

    对字符串进行统计

--- 对字符串进行统计.cc
+++ 对字符串进行统计.cc
@@ -11,13 +11,13 @@ int main()
 
     for(int i=0;i<strlen(buffer);i++)
     {
-	count[buffer[i]]++;
+	count[buffer[i]]++;//把字符的ascii码当做数组下标 count[]内容就是频率
     }
     for(int i=0;i<256;i++)
     {
         if(count[i]!=0)
         {
-            cout<<char(i)<<" "<<count[i]<<endl;
+            cout<<char(i)<<" "<<count[i]<<endl;//char(i)....char构造函数 把ascii码 转成字符
         }
 
     }
